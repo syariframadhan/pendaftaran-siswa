f83cd1b6840aadefdbe35ffb13fe999998bcc542
Syarif Ramadhan <81458964+syariframadhan@users.noreply.github.com>
2022-10-07 16:22:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+  <title>Pendaftaran Siswa Baru | Universitas PGRI Indraprasta PGRI</title>
+</head>
+
+<body>
+  <header>
+    <h3>Pendaftaran Siswa Baru</h3>
+    <h1>Universitas PGRI Indraprasta PGRI</h1>
+  </header>
+
+  <h4>Menu</h4>
+  <nav>
+    <?php if (isset($_GET['status'])) : ?>
+    <p>
+      <?php
+        if ($_GET['status'] == 'sukses') {
+          echo "Pendaftaran siswa baru berhasil";
+        } else {
+          echo "Pendaftaran gagal";
+        }
+        ?>
+    </p>
+    <?php endif; ?>
+    <ul>
+      <li><a href="form-daftar.php">Daftar Baru</a></li>
+      <li><a href="list-siswa.php">Pendaftar</a></li>
+    </ul>
+  </nav>
+
+
+</body>
+
+</html>
